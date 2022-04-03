2fd680cafad205b377cd721372577d8bae2f6101
Siddhant <santoshshrivastava39@gmail.com>
2022-04-04 01:22:42 +0530

    UC-1

--- Program.cs
+++ Program.cs
@@ -6,11 +6,28 @@ using System.Threading.Tasks;
 
 namespace AddressBookSystem
 {
-    public class Person
+    public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+            Contact list = new Contact();
+
+            Console.Write("Enter First Name: ");
+            list.FirstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            list.LastName = Console.ReadLine();
+
+            Console.Write("Enter Address : ");
+            list.Address = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            list.PhoneNumber = Console.ReadLine();
+
+            Console.Write("Enter Email: ");
+            list.Email = Console.ReadLine();
+
         }
 
     }
