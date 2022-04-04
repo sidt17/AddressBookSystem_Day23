1e5df38517e7b7a93b4da34147b5b7406ca0e148
Siddhant <santoshshrivastava39@gmail.com>
2022-04-04 17:01:37 +0530

    UC-2

--- Program.cs
+++ Program.cs
@@ -1,36 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AddressBookSystem
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
-            Contact list = new Contact();
 
-            Console.Write("Enter First Name: ");
-            list.FirstName = Console.ReadLine();
-
-            Console.Write("Enter Last Name: ");
-            list.LastName = Console.ReadLine();
-
-            Console.Write("Enter Address : ");
-            list.Address = Console.ReadLine();
-
-            Console.Write("Enter Phone Number: ");
-            list.PhoneNumber = Console.ReadLine();
-
-            Console.Write("Enter Email: ");
-            list.Email = Console.ReadLine();
-
-        }
-
+            Contact newlist = new Contact();
+            newlist.NewDetails();
+        }  
     }
-
 }
-
