baf2fdf1061f8dee8c99483780c6bc8b92d9affd
Siddhant <santoshshrivastava39@gmail.com>
2022-04-06 18:24:46 +0530

    Call in main method

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProgram
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            AddressBook obj = new AddressBook();
+            obj.Details();
+        }
+
+    }
+}
