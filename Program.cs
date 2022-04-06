ca7dd110ea93427c0db58f9f89a7946963850622
Siddhant <santoshshrivastava39@gmail.com>
2022-04-06 17:56:20 +0530

    Call in main method

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Contact addressBook = new Contact();
+            AddressBook obj = new AddressBook();
+            //getting Op in Constructor with paratmers
+            takeInputAndAddToContacts(addressBook);
+            takeInputAndAddToContacts(addressBook);
+            // called Print method 
+            obj.print();
+            Console.WriteLine("What you want to perform ? Press 1 for Edit the details ,\n Press 2 for Delete  details : ");
+            int Selectchoice = Convert.ToInt32(Console.ReadLine());
+            switch (Selectchoice)
+            {
+
+                case 1:
+                    Console.WriteLine("Enter FirstName of Contact to be edited");
+                    string firstNameOfContactToBeEdited = Console.ReadLine();
+                    Console.WriteLine("Enter LastName of Contact to be edited");
+                    string lastNameOfContactToBeEdited = Console.ReadLine();
+                    break;
+                case 2:
+                    Console.WriteLine("Enter FirstName of Contact to be deleted");
+                    string firstNameOfContactToBeDeleted = Console.ReadLine();
+                    Console.WriteLine("Enter LastName of Contact to be deleted");
+                    string lastNameOfContactToBeDeleted = Console.ReadLine();
+                    obj.Delete(firstNameOfContactToBeDeleted, lastNameOfContactToBeDeleted);
+                    break;
+                default:
+                    Console.WriteLine("Please enter the valid number : ");
+                    break;
+            }
+            Console.ReadLine();
+        }
+        // take Input And Add To Contacts
+        public static void takeInputAndAddToContacts(Contact addressBook)
+        {
+            Console.WriteLine("Enter FirstName");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter LastName");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter Zip");
+            
+            
+            
+            
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter PhoneNumber");
+            long phoneNumber = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Email id");
+            string email = Console.ReadLine();
+            
+            Console.ReadLine();
+        }
+    }
+}
